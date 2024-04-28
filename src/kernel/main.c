6c49c3aceace986d3f7c3365b42727a413c17dfa
hedi-ben-maouii <teedydeev@gmail.com>
2024-04-29 00:25:20 +0100

    update

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,9 +2,9 @@
 #include "stdio.h" 
 
 void _cdecl cstart_(){
-    puts("Hello world from C!\r\n");
-    printf("Formatted %% %c %s\r\n", 'T',"hello");
-    printf("%s","what's up\r\n");
-    printf("%d\r\n",2323);
-    printf("%x\r\n",15);
+    int a = 1; 
+    if(a)
+        printf("Hello, Wodld!\n");
+    else 
+        printf("No\n");
 }
