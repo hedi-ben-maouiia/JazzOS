627b88b2e4cbd73ec9ea071bb22050bf596404ba
hedi-ben-maouii <teedydeev@gmail.com>
2024-04-28 12:03:55 +0100

    add some functions

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,8 +2,9 @@
 #include "stdio.h" 
 
 void _cdecl cstart_(){
-    puts("Hello world from C!");
+    puts("Hello world from C!\r\n");
     printf("Formatted %% %c %s\r\n", 'T',"hello");
-    printf("%s","what's up");
-    printf("%d",2323);
+    printf("%s","what's up\r\n");
+    printf("%d\r\n",2323);
+    printf("%x\r\n",15);
 }
